43e83093b2b1550651c7831405ac6722dc6671b1
Xiangfang Chen <565499699@qq.com>
2026-08-19 12:01:29 +0800

    feat: add livecheck to omp formula for auto-update

---
+++ Formula/omp.rb
@@ -0,0 +1,49 @@
+class Omp < Formula
+  desc "Coding agent with the IDE wired in"
+  homepage "https://omp.sh"
+  version "17.3.7"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  on_macos do
+    on_arm do
+      url "https://github.com/can1357/oh-my-pi/releases/download/v#{version}/omp-darwin-arm64",
+          using: :nounzip
+      sha256 "d79a735cded3368ad6e77e988f7dc303afeb0331a2dd6ff4666e694512de2e09"
+    end
+    on_intel do
+      url "https://github.com/can1357/oh-my-pi/releases/download/v#{version}/omp-darwin-x64",
+          using: :nounzip
+      sha256 "bd94d657d89f818b89fc6f48ca1d0d3cd344942a1c26886fee6e107e687a99e2"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/can1357/oh-my-pi/releases/download/v#{version}/omp-linux-arm64",
+          using: :nounzip
+      sha256 "8c73618928f8441118eb5783e4b0480294f9741d7fe9d59bcf27eb8b4ac61bec"
+    end
+    on_intel do
+      url "https://github.com/can1357/oh-my-pi/releases/download/v#{version}/omp-linux-x64",
+          using: :nounzip
+      sha256 "4ee6ed283be10211d473e9dd45b127f2229820df3e2722a5a305b9a7f977eb83"
+    end
+  end
+
+  def install
+    bin.install Dir["omp-*"].first => "omp"
+    (bin/"omp").chmod 0555
+    with_env(HOME: buildpath) do
+      generate_completions_from_executable(bin/"omp", "completions", shells: [:bash, :zsh, :fish])
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/omp --version")
+  end
+end
