313f494f788c1ad21f2414f697865578f8210286
Xiangfang Chen <565499699@qq.com>
2026-09-17 04:25:47 +0000

    officecli 1.0.151

--- Casks/officecli.rb
+++ Casks/officecli.rb
@@ -2,11 +2,11 @@ cask "officecli" do
   arch arm: "arm64", intel: "x64"
   os macos: "mac", linux: "linux"
 
-  version "1.0.150"
-  sha256 arm:          "e669fe5a4a162f8f0f8dc871d8d85e2a15d191ec93f94426a1ecccf9d57978c8",
-         x86_64:       "3af47fd4ea86a31f228f71cf8b2a97f609784031a049aaed4ba6170606d3d0ed",
-         arm64_linux:  "93bea35aaa8f153a56a4f6e1a7d22d82aec379e42ce2d45b24b0941dcfd50968",
-         x86_64_linux: "faceb42654004f1fa5c40fb0ce641c42b7dc5a2beb270f25971ea6265b7dc227"
+  version "1.0.151"
+  sha256 arm:          "ff6834c0b4aa8a37ed78232373ed8f3acb656e1bbac4cb61ee475681cb32bec6",
+         x86_64:       "f24b200675ff88ce289e079bdaa34a16fabe425444c7b6d906541dae26745792",
+         arm64_linux:  "fccb7d7841c9ec93593a62181544c893ee184455421cf15bd76110bf0ad29c41",
+         x86_64_linux: "8e2512234ae1111e51ad3a9fadbdeca266adfa7f683773469aa45b83fe06dc7f"
 
   url "https://github.com/iOfficeAI/OfficeCLI/releases/download/v#{version}/officecli-#{os}-#{arch}"
   name "OfficeCLI"
